566a1bd48d25d8e8e69a03d65ac03719d9ca4b22
Edwar <Edwar@Blackstar>
2017-08-01 13:41:27 +0200

    Eliminar comentarios por defecto.

--- src/af_ef/AF_EF.java
+++ src/af_ef/AF_EF.java
@@ -1,21 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package af_ef;
 
-/**
- *
- * @author Edwar
- */
+
 public class AF_EF {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        
     }
     
 }
